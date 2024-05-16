4f4535bcecb7d966d267f461efab679abb2fcda2
JonathanDDavis <125523206+JonathanDDavis@users.noreply.github.com>
2024-05-15 21:42:30 -0500

    Add files via upload

---
+++ ClassAndMethods/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using ClassAndMethods;
+
+namespace ClassAndMethods
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        public bool HasPet;
+
+        public void Greeting()
+        {
+            Console.WriteLine("Hi! My name is " + Name + " and my age is " + Age);
+        }
+
+        /*public int Calc(int x, int y)
+        {
+            int number = x + y;
+            return number;
+
+        }*/
+       
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.Name = "John";
+            person.Age = 28;
+            person.HasPet = true;
+
+            person.Greeting();
+        }
+    }
+
+
+}
